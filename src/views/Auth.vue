e23f0f2695fdd9f86f27b80732605d1c91c02902
Yayanjay <yayanabdillah01@gmail.com>
2021-04-07 23:17:13 +0700

    finished

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-content>
+    <v-main>
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
           <v-col cols="12" sm="8" md="8">
@@ -103,7 +103,7 @@
           </v-col>
         </v-row>
       </v-container>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
 
